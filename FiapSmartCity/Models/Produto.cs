69095df8d8b762f0001d758a13c508a29aadb1e7
gabijesumary <gabriela.jesumary@gmail.com>
2022-11-29 17:24:27 -0300

    Add project files.

---
+++ FiapSmartCity/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿namespace FiapSmartCity.Models
+{
+    public class Produto
+    {
+        public int IdProduto { get; set; }
+        public string NomeProduto { get; set; }
+        public string Caracteristicas { get; set; }
+        public double PrecoMedio { get; set; }
+        public string Logotipo { get; set; }
+        public bool Ativo { get; set; }
+
+        //Referência para classe TipoProduto
+        public TipoProduto Tipo { get; set; }
+    }
+}
